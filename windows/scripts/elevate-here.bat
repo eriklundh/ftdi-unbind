52c74792fc0a70b0c0a20e98e39364c2370b6a4a
Erik Lundh <erik.l.lundh@gmail.com>
2026-06-04 17:23:02 +0200

    Duplicate the elevate-here.ps1 to cmd

---
+++ windows/scripts/elevate-here.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:: 1. Check for Admin rights. If not elevated, restart via PowerShell and pass the current directory path.
+fsutil dirty query %systemdrive% >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Elevating privileges...
+    powershell -Command "Start-Process cmd -ArgumentList '/k cd /d ""%~dp0""' -Verb RunAs"
+    exit /b
+)
+
+:: 2. Your batch file scripts/commands go below this line
+echo Successfully running as Administrator in the same directory!
+echo Current directory: %cd%
+
+pause
